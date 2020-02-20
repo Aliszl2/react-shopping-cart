0043f5a5152501181226974de3366aa2dbe891cd
Alison Law <alisparklaw@gmail.com>
2020-02-20 11:41:24 +0000

    Step 3- import context files into App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
+//Context
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
